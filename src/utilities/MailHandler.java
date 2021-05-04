e3ccfef9bc4aad2323a9d7086aab89f1d004788e
BuildTools <unconfigured@null.spigotmc.org>
2021-05-04 22:53:02 +0200

    MailHandler implemented, works so far, cant write äüö to file;

--- src/utilities/MailHandler.java
+++ src/utilities/MailHandler.java
@@ -1,5 +1,11 @@
 package utilities;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.file.*;
+import java.nio.*;
+
 public class MailHandler {
     private String sender;
     private String[] recipient;
@@ -34,5 +40,50 @@ public class MailHandler {
 
     public void setMailData(String mailData) {
         this.mailData = mailData;
+    }
+    // Input: String
+    // Adds one Recipient to the recipient list
+    // Must be called for every recipient
+
+    public void addRecipient(String recp) {
+        if (this.recipient == null) {
+            String[] recpStart = new String[1];
+            recpStart[0] = recp;
+            this.recipient=recpStart;
+            return;
+        }
+
+        String[] recpNew = new String[this.recipient.length+1];
+        System.arraycopy(this.recipient,0,recpNew,0,this.recipient.length);
+        recpNew[recipient.length] = recp;
+        this.recipient = recpNew;
+    }
+
+
+    public void store() throws IOException {
+        Path path = Paths.get("").toAbsolutePath();
+        path = Paths.get(path + "/mails");
+        System.out.println(path);
+
+        if (!Files.exists(path)) {
+            Files.createDirectory(path);
+        }
+
+
+        for (String recp : recipient) {
+            Path writePath = Paths.get(path +  "/" + recp);
+            if (!Files.exists(writePath)) {
+                Files.createDirectory(writePath);
+            }
+            writePath = Paths.get(writePath.toString() + "/" + sender + "_" + Math.round(Math.random()*10000)+ ".txt");
+            Files.createFile(writePath);
+            RandomAccessFile file = new RandomAccessFile(writePath.toString(), "rw");
+            ByteBuffer buffi = ByteBuffer.allocate(mailData.length());
+            buffi.put(mailData.getBytes());
+            buffi.flip();
+            FileChannel writer = file.getChannel();
+            writer.write(buffi);
+            writer.close();
+        }
     }
 }
